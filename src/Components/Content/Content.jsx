3b35bc86ebde331dcc41919bde240b6519424ecd
cedvinas <cyzauskas@gmail.com>
2022-07-08 14:55:50 +0300

    Updated navbar

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -3,17 +3,8 @@ import './Content.scss'
 
 const Content = () => {
 
-    const StickyNav = () => {
-        // const nav = document.getElementById('nav');
-        // nav.style.backgroundColor = 'black';
-        // nav.style.position = 'sticky';
-        // console.log('scroll');
-    };
-
-
-
     return (
-        <div id="content" onScroll={StickyNav()}>
+        <div id="content">
 
         </div>
     )
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,30 @@
+import { logDOM } from "@testing-library/react";
 import React from "react";
 import './Nav.scss'
 
 const Nav = () => {
 
+    const StickyNav = () => {
+        window.addEventListener('scroll', (e) => {
+            const nav = document.getElementById('nav');
+            const logo = document.getElementById('logo')
+            if (window.scrollY > 0) {
+                nav.classList.add('nav-sticky')
+                logo.src = 'https://per4mmedia.com/wp-content/uploads/2021/03/per4mmedia.com-white-200x48.png'
+            } else {
+                nav.classList.remove('nav-sticky')
+                logo.src = 'https://per4mmedia.com/wp-content/uploads/2021/03/per4mmedia.com-black-250x59.png'
+            }
+        })
+    };
+
+    StickyNav();
 
     return (
         <nav id="nav">
             <div className="container">
                 <div>
-                    <img src="https://per4mmedia.com/wp-content/uploads/2021/03/per4mmedia.com-black-250x59.png" alt="logo" />
+                    <img id="logo" src="https://per4mmedia.com/wp-content/uploads/2021/03/per4mmedia.com-black-250x59.png" alt="logo" />
                 </div>
 
                 <div className="nav-links">
